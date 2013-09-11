74fd89b08a84c733455068c4f3795e5316a70d0e
Landon Noss <landon.noss@ff0000.com>
2013-09-10 17:07:23 -0700

    step 2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var App = Ember.Application.create({
-	// LOG_TRANSITIONS: true,
- //    LOG_VIEW_LOOKUPS: true,
- //    LOG_ACTIVE_GENERATION: true
+	LOG_TRANSITIONS: true,
+    LOG_VIEW_LOOKUPS: true,
+    LOG_ACTIVE_GENERATION: true
 });
 
 App.Router.map(function() {
@@ -11,9 +11,28 @@ App.Router.map(function() {
 });
 
 App.PostsRoute = Ember.Route.extend({
-
+	data: null,
+	subreddit: null,
+	model: function(params) {
+		if (!this.data || this.subreddit !== params.reddit_id) {
+			this.subreddit = params.reddit_id;
+			this.data = Ember.$.getJSON('http://www.reddit.com/r/' + this.subreddit + '/.json?jsonp=?').then(function(response) {
+				var posts = Em.A();
+				response.data.children.forEach(function (post) {
+					posts.push(post.data);
+				});
+				console.log(posts);
+				return posts;
+			});
+		}
+		return this.data;
+	}
 });
 
 App.PostRoute = Ember.Route.extend({
-
+	model: function(params) {
+		var parent = this.modelFor('posts'),
+			post = parent.findBy('id', params.post_id);
+		return post;
+	}
 });
